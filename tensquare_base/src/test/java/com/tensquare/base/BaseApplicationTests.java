3e398c2b9b078ee0c8fc8149464854542457324a
xjl1995822 <18279148786@163.com>
2020-05-14 11:08:42 +0800

    Springcloud_base工程的基本构建

--- tensquare_base/src/test/java/com/tensquare/base/BaseApplicationTests.java
+++ tensquare_base/src/test/java/com/tensquare/base/BaseApplicationTests.java
@@ -1,10 +1,10 @@
-package com.shanghaiuniversity.spingcloudbese;
+package com.tensquare.base;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcloudbaseApplicationTests {
+class BaseApplicationTests {
 
     @Test
     void contextLoads() {
