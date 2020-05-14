77b7c52574b710fb658a41845beb6c9d8f8321fa
xjl1995822 <18279148786@163.com>
2020-05-14 10:57:51 +0800

    Springcloud_base工程的基本构建

---
+++ SpingCloud_bese/src/test/java/com/shanghaiuniversity/spingcloudbese/SpringcloudbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanghaiuniversity.spingcloudbese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
