b0fd0cab4543f949d659882152ba47ace8b40ea9
xjl1995822 <18279148786@163.com>
2020-05-14 15:16:32 +0800

    Springcloud_base工程的基本构建

--- tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -2,12 +2,25 @@ package com.tensquare.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * Copyright (C), 2018-2020
+ * FileName: com.tensquare.base.BaseApplication
+ * Author:   xjl
+ * Date:     2020/5/14 12:36
+ * Description: 启动类
+ */
 
 @SpringBootApplication
 public class BaseApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(BaseApplication.class, args);
+        SpringApplication.run(BaseApplication.class);
     }
 
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
 }
